1115af9ac244d76c6cc26d1b2f6d7deef816b366
Olaf0321 <daikifujimoto344@gmail.com>
2025-04-28 11:09:05 +0900

    fix-run-error

--- backend/main.py
+++ backend/main.py
@@ -19,3 +19,7 @@ app.include_router(tables.router, prefix="/tables", tags=["tables"])
 app.include_router(columns.router, prefix="/columns", tags=["columns"])
 app.include_router(child.router, prefix="/child", tags=["child"])
 app.include_router(list_column.router, prefix="/list_column", tags=["list_column"])
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Welcome to the SQLite CRUD API."}
